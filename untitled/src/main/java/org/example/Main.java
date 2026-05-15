4325c040a71ad7ccaed50a07539ba2a062b9fde5
Afranio Caires <afraniomcaires@gmail.com>
2026-05-15 19:00:34 -0300

    feat: add support for directory watch

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -1,17 +1,25 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-  static void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import org.example.config.Config;
+import org.example.monitor.LogMonitor;
+import org.example.processor.LogProcessor;
 
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      IO.println("i = " + i);
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        LogProcessor mockProcessor = logLine -> {
+            System.out.println("[Pessoa B Mock] Recebido: " + logLine);
+        };
+
+        String directoryToWatch = Config.getMonitorDirectory();
+        LogMonitor monitor = new LogMonitor(directoryToWatch, mockProcessor);
+
+        try {
+            monitor.start();
+        } catch (IOException | InterruptedException e) {
+            System.err.println("Erro ao iniciar monitor: " + e.getMessage());
+            Thread.currentThread().interrupt();
+        }
     }
-  }
 }
